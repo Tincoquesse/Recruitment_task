00e611c9d4c4f3cb439052a95078dfa782493bd9
Tincoquesse <kamil.sound@gmail.com>
2022-08-26 14:16:36 +0200

    Initial commit

---
+++ src/main/java/com/atipera/recruitment_task/api/RestSpringbootController.java
@@ -0,0 +1,34 @@
+package com.atipera.recruitment_task.api;
+
+import com.atipera.recruitment_task.model.BranchResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api")
+public class RestSpringbootController {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @GetMapping("/user/{username}")
+    public Object[] getUserData(@PathVariable String username) {
+        String uri = "https://api.github.com/users/" + username + "/repos";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Object[]> response = restTemplate.getForEntity(uri, Object[].class);
+        Object[] objects = response.getBody();
+
+        return objects;
+
+    }
+
+}
---
+++ src/main/java/com/atipera/recruitment_task/service/RestSpringbootService.java
@@ -0,0 +1,7 @@
+package com.atipera.recruitment_task.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RestSpringbootService {
+}
