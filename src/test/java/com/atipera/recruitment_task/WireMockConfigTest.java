c1501ccebf63e62a2349b90aa5133bf0fca03c23
Tincoquesse <kamil.sound@gmail.com>
2022-08-31 12:23:48 +0200

    defeat

--- src/test/java/com/atipera/recruitment_task/WireMockConfigTest.java
+++ src/test/java/com/atipera/recruitment_task/WireMockConfigTest.java
@@ -1,6 +1,8 @@
 package com.atipera.recruitment_task;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +13,12 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @SpringBootTest
 public class WireMockConfigTest {
-
-    WireMockServer wireMockServer = new WireMockServer();
+    @Rule
+    WireMockRule wireMockRule = new WireMockRule();
 
     @Test
     void test(){
+        WireMockServer wireMockServer = new WireMockServer();
         wireMockServer.start();
         RestTemplate restTemplate = new RestTemplate();
 
@@ -24,6 +27,7 @@ public class WireMockConfigTest {
         ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/api", String.class);
 
         assertThat(response.getBody()).isEqualTo("Welcome to Baeldung!");
+        wireMockServer.stop();
 
 
     }
