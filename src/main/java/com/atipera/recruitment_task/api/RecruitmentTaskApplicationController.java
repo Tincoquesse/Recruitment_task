fd20928a379ac3f049747a23eb6327668bdbbb47
kamilkotlarz <kamil.sound@gmail.com>
2022-09-04 00:41:25 +0200

    deffff

--- src/main/java/com/atipera/recruitment_task/api/RecruitmentTaskApplicationController.java
+++ src/main/java/com/atipera/recruitment_task/api/RecruitmentTaskApplicationController.java
@@ -1,14 +1,20 @@
 package com.atipera.recruitment_task.api;
 
+import com.atipera.recruitment_task.model.Repo;
 import com.atipera.recruitment_task.model.RepoDTO;
 import com.atipera.recruitment_task.service.RecruitmentTaskApplicationService;
+import com.atipera.recruitment_task.service.RepoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -31,24 +37,23 @@ public class RecruitmentTaskApplicationController {
         return new ResponseEntity<>(reposForResponse, HttpStatus.OK);
     }
 
-//    @GetMapping("/repos")
-////    public Flux<RepoDTO> getRepos (){
-//
-//        List<String> repoNames = new ArrayList<>();
-//        repoNames.add("Alledrogo_Frontend");
-//        repoNames.add("Recruitment_task ");
-//
-//        Flux<Repo> repos = Flux.fromIterable(repoNames).flatMap(x -> {
-//            Mono<List<Repo>> repoFlux = webClientBuilder.build()
-//                    .get()
-//                    .uri("https://api.github.com/repos/Tincoquesse/{repoName}/branches", x)
-//                    .retrieve()
-//                    .bodyToMono(Repo[].class)
-//                    .map(Arrays::asList)
-//
-//            return repoFlux;
-//
-//        });
-//        return repos.map(RepoMapper::fromEntity);
-//    }
+    @GetMapping("/repos")
+    public Flux<RepoDTO> getRepos (){
+
+        List<String> repoNames = new ArrayList<>();
+        repoNames.add("Alledrogo_Frontend");
+        repoNames.add("Recruitment_task");
+
+        Flux<Repo> repos = Flux.fromIterable(repoNames).flatMap(x -> {
+            return webClientBuilder.build()
+                    .get()
+                    .uri("https://api.github.com/repos/Tincoquesse/{repoName}/branches", x)
+                    .retrieve()
+                    .bodyToMono(Repo[].class)
+                    .map(Arrays::asList)
+                    .flatMapIterable(ls->ls);
+        });
+        System.out.printf(repos.toString());
+        return repos.map(RepoMapper::fromEntity);
+    }
 }
